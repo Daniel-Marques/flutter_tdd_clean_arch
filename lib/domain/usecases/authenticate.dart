f53ae7d765d27468165fce9ec441a1f80d1a6fe2
Daniel Marques <daniel.silva.city@gmail.com>
2021-08-11 15:45:02 -0300

    🪄 Perfumery Imports

--- lib/domain/usecases/authenticate.dart
+++ lib/domain/usecases/authenticate.dart
@@ -1,5 +1,6 @@
-import 'package:flutter_tdd_clean_arch/domain/entities/account_entities.dart';
 import 'package:meta/meta.dart';
+
+import '../entities/account_entities.dart';
 
 abstract class Authentication {
   Future<AccountEntity> auth({
